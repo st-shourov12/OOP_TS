c27b175c4be3e829406500f4ce58e2e2d419b146
st-shourov12 <shourovsc38@gmail.com>
2026-05-06 17:18:13 +0600

    implementing polymorphism

--- src/polymorphism.ts
+++ src/polymorphism.ts
@@ -27,4 +27,48 @@ const person1 = new Person();
 const person2 = new Student();
 const person3 = new Developer();
 
-getSleepingHours(person3)
+getSleepingHours(person3);
+
+
+
+class Shape {
+    getArea() {
+        return 0;
+    }
+}
+
+class Circle extends Shape {
+    radius : number ;
+
+    constructor (radius: number){
+        super()
+        this.radius = radius
+    }
+
+    getArea(): number {
+        return Math.PI * this.radius * this.radius
+    }
+}
+
+class Rectangle extends Shape {
+
+    constructor(public height : number , public width : number){
+        super()
+    }
+
+    getArea(): number {
+        return this.height * this.width
+    }
+}
+
+
+const getArea = (param : Shape) => {
+    console.log(param.getArea());
+}
+
+const area1 = new Shape();
+const area2 = new Circle(5);
+const area3 = new Rectangle(20,40);
+
+getArea(area2)
+
