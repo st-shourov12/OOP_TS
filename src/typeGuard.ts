06a9d9a246ccfdc9418af898414e5b4398c9d22d
st-shourov12 <shourovsc38@gmail.com>
2026-05-06 13:52:04 +0600

    learning type guard

---
+++ src/typeGuard.ts
@@ -0,0 +1,15 @@
+// ts-node ./src/typeGuard.ts
+
+type Alphaneumeric = number | string ;
+
+const add = (num1 : Alphaneumeric , num2:Alphaneumeric) => {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2
+    }else {
+       return num1.toString()  + num2.toString()
+    }
+}
+
+console.log(add(5,9));
+console.log(add(5,'9'));
+console.log(add('5','9'));
